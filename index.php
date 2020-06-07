ab8f9092fb6855854d9fbd75d0dd503454e895fe
kmahesh27 <kmahesh81@gmail.com>
2020-06-07 23:50:57 +0530

    test index created

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Home Page/</title>
+	</head>
+	<body>
+	    <h1>Welcome to ABCBank</h1>
+	    <h2>Candidate Information Form</h2>
+	    <h3>Contact to ABC Bank only between 10:00 am to 3:00 pm</h3>>
+	    <ul>
+		<?php
+  		    $json = file_get_contents('http://product-service');
+  		    $obj = json_decode($json);
+		    $products = $obj->products;
+		    foreach ($products as $product) {
+			echo "<li>$product</li>";
+		    }
+    		?>
+	    </ul>
+	</body>
+ </html>
