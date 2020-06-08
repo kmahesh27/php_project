f4243b7ef9c6517bd2940b4eba4d9571e483b526
kmahesh27 <kmahesh81@gmail.com>
2020-06-08 16:38:07 +0530

    UPDATED INDEX FILE

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 		<title>Home Page/</title>
 	</head>
 	<body>
-	    <h1>Welcome to YES Bank</h1>
+	    <h1>Welcome to CITI Bank</h1>
 	    <h2>Candidate Information Form</h2>
-	    <h3>Contact to YES Bank only between 10:00 am to 3:00 pm</h3>>
+	    <h3>Contact to CITI Bank only between 10:00 am to 3:00 pm</h3>>
 	    <ul>
 		<?php
   		    $json = file_get_contents('http://product-service');
