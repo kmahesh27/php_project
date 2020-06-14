9cb213ec099a0357bf81fb93e186abfff37e08e6
kmahesh27 <kmahesh81@gmail.com>
2020-06-14 19:55:28 +0530

    updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<title>Home Page/</title>
 	</head>
 	<body>
-	    <h1>Welcome to CITI-Bank 2</h1>
+	    <h1>Welcome to CITI-Bank 22</h1>
 	    <h2>Candidate Information Form</h2>
 	    <h3>Contact to CITI Bank only between 10:00 am to 3:00 pm</h3>>
 	    <ul>
